b9fb5e090c75f2e15ffc9b91ddfaf212aa377182
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-02-24 20:19:45 +0200

    exer 1

--- exer1_create_ppm/exer1_create_ppm.c
+++ exer1_create_ppm/exer1_create_ppm.c
@@ -1,23 +1,34 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-# define SIZE 256
+#define SIZE 256
 
+// create a PPM image with a gradient from black to white
 int main(void)
 {
-    int fd = open("../output/first.ppm", 1);
-    printf("P3\n\n%d\n", SIZE);
-    
-    for (int i = 0; i < SIZE; ++i)
+    // open file
+    int fd = open("../output/first.ppm", O_CREAT | O_WRONLY, 0644);
+    if(fd < 0)
     {
-        for (int j = 0; j < SIZE; ++j)
-        {
-            double r = j / (SIZE - 1) * SIZE;
-            double g = i / (SIZE - 1) * SIZE;
-            double b = 0.0;
-        }
+        perror("open");
+        return EXIT_FAILURE;
     }
 
+    // header
+    dprintf(fd, "P3\n%d %d\n%d\n", SIZE, SIZE, SIZE - 1);
 
+    // body
+    for (int i = 0; i < SIZE; ++i)
+    {
+        printf("Lines remain: %d\n", SIZE - i);
+        for (int j = 0; j < SIZE; ++j)
+            dprintf(fd, "%d %d %d\n", j, i, 0);
+    }
+    printf("Done.\n");
+
+    // close file
+    close(fd);
+    return EXIT_SUCCESS;
 }
