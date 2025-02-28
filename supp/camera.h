7681e651ad256f51c57bdd326adda2fe68614e47
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-02-28 23:05:44 +0200

    antialiasing

--- supp/camera.h
+++ supp/camera.h
@@ -7,8 +7,10 @@ typedef struct s_camera
 {
     int image_width;
     float aspect_ratio;
+    int samples_per_pixel;
 
     int image_height;
+    float pixel_samples_scale;
     t_point3 camera_center;
     t_point3 pixel00_loc;
     t_vec3 pixel_delta_u;
@@ -16,6 +18,7 @@ typedef struct s_camera
 } t_camera;
 
 t_camera camera_new(float aspect_ratio, float image_width);
+t_camera camera_new_aa(float aspect_ratio, float image_width, int samples_per_pixel);
 void camera_render(int fd, t_camera *camera, t_hittable_arr *world);
 
 #endif
