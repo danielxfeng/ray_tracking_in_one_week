1e6b4ceb5df781715e7ca3d876d3fb189c2f792f
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-03-01 16:30:10 +0200

    Lambertian and Gramma.

--- supp/color.c
+++ supp/color.c
@@ -1,13 +1,21 @@
 #include "color.h"
 #include "interval.h"
 #include <stdio.h>
+#include <math.h>
+
+static float linear_to_gamma(float linear)
+{
+    if (linear <= 0.0)
+        return 0.0;
+    return sqrt(linear);
+}
 
 void write_color(int fd, t_color *pixel_color)
 {
     t_interval interval = {0.0, 0.999};
 
     dprintf(fd, "%d %d %d\n",
-        (int)(MAX_COLOR * interval_clamp(&interval, pixel_color->x)),
-        (int)(MAX_COLOR * interval_clamp(&interval, pixel_color->y)),
-        (int)(MAX_COLOR * interval_clamp(&interval, pixel_color->z)));
+        (int)(MAX_COLOR * interval_clamp(&interval, linear_to_gamma(pixel_color->x))),
+        (int)(MAX_COLOR * interval_clamp(&interval, linear_to_gamma(pixel_color->y))),
+        (int)(MAX_COLOR * interval_clamp(&interval, linear_to_gamma(pixel_color->z))));
 }
